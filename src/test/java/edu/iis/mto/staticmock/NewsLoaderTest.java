518437313e0744db4020f80879f417f48cdfbb62
Aleksander Kaczmarczyk <axelkko@gmail.com>
2017-04-04 00:54:17 +0200

    NewsLoaderTest - dodanie wstepnych importow i adnotacji

--- src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
+++ src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
@@ -4,7 +4,17 @@ import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+import static org.powermock.api.mockito.PowerMockito.*;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest()
 public class NewsLoaderTest {
 
 	@Before
