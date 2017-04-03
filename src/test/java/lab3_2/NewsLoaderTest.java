026fd6d240f33db32d19dd0e1be00957650d61a4
Aleksander Kaczmarczyk <axelkko@gmail.com>
2017-04-04 00:41:10 +0200

    NewsLoaderTest - utworzenie klasy testowej

---
+++ src/test/java/lab3_2/NewsLoaderTest.java
@@ -0,0 +1,19 @@
+package lab3_2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class NewsLoaderTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
